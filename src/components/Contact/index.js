fb9aecf37cb6a6db995d176091ee9448dc485c41
Michael Dreesen <michaeldreesen90@gmail.com>
2020-11-29 21:56:57 -0700

    added error validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,40 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+    const [errorMessage, setErrorMessage] = useState('');
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
     // the function will sync the internal state of the component "formState" with the user input form the DOM
     // the onChange event listener will ensure that teh "handleChange" 
     function handleChange(e) {
+
+        if(e.target.value === 'email') {
+
+            // this conditional says if the <input> is email, then validate the value of the input field with the validEmail function and assign its return to "isValid"
+
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid condition statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+                // the nested conditional statement checks whether the values of these elements are blank
+                // if the input values are blank, an error message is assigned to "errorMessage", if not "errorMessage" is set to an empty string, meaning there is no error
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+            }
+
+            if (!errorMessage) {
+                setFormState({...formState, [e.target.name]: e.target.value })
+            }
+            console.log('errorMessage', errorMessage);
+        }
+
         // using setFormState to update the formState value for the name property
         // assigned the value taken from the input field in the UI with e.target.value and assigned this value to the property "formState.name"
         // use the spread operator "...formState" so we can retain the other key value paris in this object
@@ -16,6 +44,8 @@ function ContactForm() {
         // name property of target in the preceding expression actually refers to the name attribute of the form element
         // this attribute value matches the property names of "formSate (name, email, message)" and allows us to use [] to create dynamic properties
         setFormState({...formState, [e.target.name]: e.target.value })
+        console.log('errorMessage', errorMessage);
+
     }
 
     // located outside of the "handleChange" function declaration - because the asynchronous nature og the setFormState function will cause the console.log - 
